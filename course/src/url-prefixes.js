f6c368572a7a7b7c34517ff7c024a3fe7b3058cc
JAJAmoa <JJ@hgeek.net>
2020-12-17 01:45:15 +0800

    feat(proxy): set API prefix for localhost backend

--- course/src/url-prefixes.js
+++ course/src/url-prefixes.js
@@ -1,3 +1,7 @@
 export const API_CONTEST_PREFIX = '/api/v1/contest'
 
 export const API_CONTEST_QUESTIONS_PREFIX = '/api/v1/contest-question-database'
+
+export const API_USER_PREFIX = '/api/v1/user'
+
+export const API_COURSE_PREFIX = '/api/v1/course'
