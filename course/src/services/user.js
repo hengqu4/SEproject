ac09a07b665fc6dcd1757c80b4bc4afec0a3decf
JAJAmoa <JJ@hgeek.net>
2020-12-17 02:53:33 +0800

    feat(user info zone): applied the sync of the user info and currentUser state

--- course/src/services/user.js
+++ course/src/services/user.js
@@ -1,11 +1,21 @@
 import request from '@/utils/request'
+import { API_USER_PREFIX } from '@/url-prefixes'
 
 export async function query() {
   return request('/api/users')
 }
+// export async function queryCurrent() {
+//   return request('/api/currentUser')
+// }
 export async function queryCurrent() {
-  return request('/api/currentUser')
+  return request('/account/', {
+    method: 'GET',
+    prefix: API_USER_PREFIX,
+  }).catch(function (error) {
+    console.log(error)
+  })
 }
+
 export async function queryNotices() {
   return request('/api/notices')
 }
